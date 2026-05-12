432aa75d02719e301471fa26a3c58146d8866524
ignaciovillar10 <ignaciovillaar@gmail.com>
2026-05-12 15:01:38 -0300

    Delete sistemaliga_tpe directory

--- sistemaliga_tpe/app/view/equipos.view.php
+++
@@ -1,7 +0,0 @@
-<?php
-class EquiposView {
-    public function renderequipos($equipos,$ligas, $idLiga = null,$ligaNombre = 'todas las ligas', $equipoToEdit = null): void {
-        $count = count($equipos);
-        require __DIR__ . '/templates/equipos.phtml';
-    }
-}
--- sistemaliga_tpe/app/view/ligas.view.php
+++
@@ -1,7 +0,0 @@
-<?php
-class LigasView {
-    public function renderligas(array $ligas, $ligaToEdit = null): void {
-        $count = count($ligas);
-        require __DIR__ . '/templates/ligas.phtml';
-    }
-}
