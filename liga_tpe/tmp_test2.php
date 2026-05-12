65f12f84391a0d293e266f03ecad9c04cc8d1e3d
ignaciovillar10 <ignaciovillaar@gmail.com>
2026-05-12 10:00:15 -0300

    Delete liga_tpe directory

--- liga_tpe/tmp_test2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=sistemaliga_tpe;charset=utf8', 'root', '');
-foreach (['equipos', 'ligas'] as $t) {
-    $q = $db->query('DESCRIBE ' . $t);
-    echo 'TABLE ' . $t . ':\n';
-    print_r($q->fetchAll(PDO::FETCH_COLUMN));
-}
